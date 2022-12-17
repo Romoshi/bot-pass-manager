6b660d828b510b2e541254003d2f88b224b3a2ae
Romoshi <derinrom@gmail.com>
2022-12-17 16:53:13 +0300

    fix

--- src/main/java/edu/romoshi/commands/Commands.java
+++ src/main/java/edu/romoshi/commands/Commands.java
@@ -1,6 +1,5 @@
 package edu.romoshi.commands;
 
-import edu.romoshi.CreatingFileAcc;
 import edu.romoshi.crypto.PassCipher;
 import edu.romoshi.userTools.AccWhichSave;
 
@@ -11,11 +10,6 @@ public class Commands {
     public static void addAccount(AccWhichSave account) throws Exception {
         PassCipher passSave = new PassCipher();
         passSave.init();
-
-        String saving = account.getNameService() + "  "
-                        + account.getLogin() + "  "
-                        + passSave.encrypt(account.getPassword());
-        CreatingFileAcc.Creating(saving);
     }
     public static void deleteAccount() {}
     public static void changeMasterKey() {}
