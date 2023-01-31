8eec760493e221404ce1da46aa220576fad0e749
Romoshi <derinrom@gmail.com>
2023-01-31 16:51:34 +0500

    some upd

--- src/main/java/edu/romoshi/database/DBUtils.java
+++ src/main/java/edu/romoshi/database/DBUtils.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtils {
-    private static String dbUrl = "jdbc:mysql://localhost:3306/test";
-    private static String dbUser = "root";
-    private static String dbPassword = "root";
+    private static String dbUrl = System.getenv("DB_URL");
+    private static String dbUser = System.getenv("DB_USER");
+    private static String dbPassword = System.getenv("DB_PASS");
     public static Connection connection;
     public static Connection getNewConnection() throws SQLException {
         connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
