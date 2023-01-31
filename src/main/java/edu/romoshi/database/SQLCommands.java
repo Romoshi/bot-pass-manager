49b84c713049d14366e4c7b8d3207fa489ce7798
Romoshi <derinrom@gmail.com>
2023-01-31 17:25:03 +0500

    upd db package

--- src/main/java/edu/romoshi/database/SQLCommands.java
+++ src/main/java/edu/romoshi/database/SQLCommands.java
@@ -5,9 +5,10 @@ public class SQLCommands {
     public static final String READ = "SELECT * FROM db_passwords";
     public static final String DELETE = "DELETE FROM db_passwords WHERE name_service = ?";
 
-    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS db_passwords(" +
-                                    "id BIGINT AUTO_INCREMENT PRIMARY KEY," +
-                                    "name_service VARCHAR(60) NOT NULL," +
-                                    "login VARCHAR(60) NOT NULL," +
-                                    "password VARCHAR(100) NOT NULL);";
+    public static final String CREATE_TABLE = """
+                                                CREATE TABLE IF NOT EXISTS db_passwords(
+                                                id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                                name_service VARCHAR(60) NOT NULL,
+                                                login VARCHAR(60) NOT NULL,
+                                                password VARCHAR(100) NOT NULL);""";
 }
