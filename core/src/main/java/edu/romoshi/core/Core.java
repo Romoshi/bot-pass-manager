8a4187dc33bdeb9ab0a3a06ea6a1010315538a39
romoshi <derinrom@gmail.com>
2023-04-09 14:46:10 +0500

    Core gRPC is done!

--- core/src/main/java/edu/romoshi/core/Core.java
+++ core/src/main/java/edu/romoshi/core/Core.java
@@ -1,23 +1,30 @@
 package edu.romoshi.core;
 
+import edu.romoshi.core.dao.Tables;
 import edu.romoshi.core.grpc.AccountsServiceGrpc;
 import edu.romoshi.core.grpc.UsersServiceGrpc;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 public class Core {
+    private static final Logger logger = LoggerFactory.getLogger(Core.class);
     public static void main(String[] args) throws InterruptedException, IOException {
-//        if(!Tables.isFlag()) {
-//            Tables.initTables();
-//        }
-//        Server core = ServerBuilder
-//                .forPort(8080)
-//                .addService(new AccountsServiceGrpc())
-//                .addService(new UsersServiceGrpc())
-//                .build();
-//
-//        server.start();
-//        server.awaitTermination();
+        Server core = ServerBuilder
+                .forPort(8080)
+                .addService(new AccountsServiceGrpc())
+                .addService(new UsersServiceGrpc())
+                .build();
+        if(!Tables.isFlag()) {
+            Tables.initTables();
+        }
+
+        logger.info("Server started!");
+        core.start();
+        core.awaitTermination();
     }
 }
